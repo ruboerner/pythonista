bdd99a56d6c51024e868a744de94a3056c00355c
Ralph-Uwe Börner <rub@geophysik.tu-freiberg.de>
2016-01-17 22:24:28 +0100

    Working

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 Find prime numbers.
 """
 
-import numpy as np
+def factors(n):
+    gaps = [1,2,2,4,2,4,2,4,6,2,6]
+    length, cycle = 11, 3
+    f, fs, next = 2, [], 0
+    while f * f <= n:
+        while n % f == 0:
+            fs.append(f)
+            n /= f
+        f += gaps[next]
+        next += 1
+        if next == length:
+            next = cycle
+    if n > 1: fs.append(n)
+    return fs
+
+print factors(112)
